e2d95276ff14d6c0a8cabf79c9cb43c08efe16f8
Rohan Pagare <rohanpagare7599@gmail.com>
2024-09-14 14:16:13 +0530

    add tilt in hero section

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,31 +1,42 @@
 import styles from './HeroStyles.module.css';
-import heroImg from '../../assets/hero-img.png'
 import ogHeroImg from '../../assets/og-img.jpg'
 import sun from '../../assets/sun.svg'
 import moon from '../../assets/moon.svg'
-import twitterLight from '../../assets/twitter-light.svg'
 import leetcodeLight from '../../assets/leetcode-light.svg'
 import leetcodeDark from '../../assets/leetcode-dark.svg'
 import githubLight from '../../assets/github-light.svg'
 import linkedinLight from '../../assets/linkedin-light.svg'
-import twitterDark from '../../assets/twitter-dark.svg'
 import githubDark from '../../assets/github-dark.svg'
 import linkedinDark from '../../assets/linkedin-dark.svg'
 import cv from '../../assets/rohan-pagare-cv.pdf'
 import { useTheme } from '../../common/ThemeContext';
+import { Tilt } from 'react-tilt'
+
+const defaultOptions = {
+    reverse: false,  // reverse the tilt direction
+    max: 35,     // max tilt rotation (degrees)
+    perspective: 1000,   // Transform perspective, the lower the more extreme the tilt gets.
+    scale: 1.1,    // 2 = 200%, 1.5 = 150%, etc..
+    speed: 1000,   // Speed of the enter/exit transition
+    transition: true,   // Set a transition on enter/exit.
+    axis: null,   // What axis should be disabled. Can be X or Y.
+    reset: true,    // If the tilt effect has to be reset on exit.
+    easing: "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+}
 
 function Hero() {
     const { theme, toggleTheme } = useTheme();
     const themeIcon = theme == 'light' ? sun : moon;
-    const twitterIcon = theme == 'light' ? twitterLight : twitterDark;
     const leetcodeIcon = theme == 'light' ? leetcodeDark : leetcodeLight;
     const githubIcon = theme == 'light' ? githubLight : githubDark;
     const linkedinIcon = theme == 'light' ? linkedinLight : linkedinDark;
     return (
         <section id="hero" className={styles.container}>
+            <img className={styles.colorMode} src={themeIcon} onClick={toggleTheme} alt='color mode img' />
             <div className={styles.colorModeContainer}>
-                <img className={styles.hero} src={ogHeroImg} alt='profile img' />
-                <img className={styles.colorMode} src={themeIcon} onClick={toggleTheme} alt='color mode img' />
+                <Tilt options={defaultOptions} >
+                    <img className={styles.hero} src={ogHeroImg} alt='profile img' />
+                </Tilt>
             </div>
             <div className={styles.info}>
                 <h1>Rohan
