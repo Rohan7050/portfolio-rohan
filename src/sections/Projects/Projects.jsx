e81dca01200771f2fdbff725eb1b0de8b7e4249a
Rohan Pagare <rohanpagare7599@gmail.com>
2025-01-26 11:08:07 +0530

    change order of projects

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -6,9 +6,9 @@ import KotakGlobal from '../../assets/kotak-international.png'
 import ProjectCard from '../../common/components/ProjectCard'
 function Projects() {
     const projectList = [
-        { id: 1, name: 'Upvote', desc: '', link: 'https://github.com/Rohan7050/upvote', src: Upvote },
         { id: 2, name: 'KotakMF', desc: '', link: 'https://www.kotakmf.com/', src: KotakMF },
         { id: 3, name: 'Kotak Global', desc: '', link: 'https://www.kotak-international.com/', src: KotakGlobal },
+        { id: 1, name: 'Upvote', desc: '', link: 'https://github.com/Rohan7050/upvote', src: Upvote },
         {id: 4, name: 'Tic Tac Toe', desc: '', link: 'https://github.com/Rohan7050/tic-tac-toe', src: TicTacToe}
     ]
     return (
